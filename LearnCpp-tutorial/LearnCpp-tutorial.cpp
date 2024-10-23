836755aa74b1094ddb9e054b09f22e49216a8c17
Vagvolgyi Balazs <vagvolgyibalazsrobert@gmail.com>
2024-10-23 10:12:34 +0200

    Add project files.

---
+++ LearnCpp-tutorial/LearnCpp-tutorial.cpp
@@ -0,0 +1,12 @@
+﻿// LearnCpp-tutorial.cpp : Defines the entry point for the application.
+//
+
+#include "LearnCpp-tutorial.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
